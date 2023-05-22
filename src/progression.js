81f3ceb7f2669d263d8072a6efa62f7311534111
root <root@WIN-ETHDOG054HU>
2023-05-22 17:21:18 +0300

    eslint --fix

--- src/progression.js
+++ src/progression.js
@@ -2,26 +2,27 @@
 import readlineSync from 'readline-sync';
 
 // eslint-disable-next-line max-len
-const arithmeticProgression = (n, lim) => Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n); // взято у друга
+const arithmeticProgression = (n, lim) => Array.from({ length: Math.ceil(lim / n) }, (_, i) => (i + 1) * n);// https://github.com/30-seconds/30-seconds-of-code/blob/master/snippets/arithmeticProgression.md
 
-export default function Main() {
+// eslint-disable-next-line consistent-return
+export default function progression() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('What number is missing in the progression?');
-  let emptyAnsw = 1;
-  while (emptyAnsw <= 3) {
+  let rounds = 1;
+  while (rounds <= 3) {
     // eslint-disable-next-line max-len
-    const randonCount = arithmeticProgression(Math.floor(Math.random() * (20 - 10)) + 2, Math.floor(Math.random() * (200 - 50)) + 50);
-    const trytrue = randonCount[3];
-    randonCount[3] = '..';
-    console.log(`Question: ${randonCount.join(' ')}`);
-    const newName = readlineSync.question('You answer: ');
-    if (Number(newName) !== Number(trytrue)) {
-      console.log(`'${newName}' is wrong answer ;(. Correct answer was '${trytrue}'.`);
-      return console.log(`Let's try again, ${newName}!`);
+    const line = arithmeticProgression(Math.floor(Math.random() * (20 - 10)) + 2, Math.floor(Math.random() * (200 - 50)) + 50);
+    const trueNumber = line[3];
+    line[3] = '..';
+    console.log(`Question: ${line.join(' ')}`);
+    const userAnswer = readlineSync.question('You answer: ');
+    if (Number(userAnswer) !== Number(trueNumber)) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueNumber}'.`);
+      return console.log(`Let's try again, ${name}!`);
     }
-    emptyAnsw += 1;
+    rounds += 1;
     console.log('Correct!');
   }
 
